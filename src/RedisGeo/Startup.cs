69b0db6b72d3db8b912b12808a3dd4a89f4714a2
Todd Robertson <todd@kwerius.com>
2019-12-22 21:28:01 -0800

    Added custom logging provider to log timestamp

--- src/RedisGeo/Startup.cs
+++ src/RedisGeo/Startup.cs
@@ -25,7 +25,6 @@ namespace RedisGeo
     public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
     {
       loggerFactory.AddConsole();
-      loggerFactory.AddProvider(new CustomLoggerProvider());
 
       if (env.IsDevelopment())
       {
@@ -36,46 +35,6 @@ namespace RedisGeo
       {
         AppSettings = new NetCoreAppSettings(Configuration)
       });
-    }
-  }
-
-  public class CustomLoggerProvider : ILoggerProvider
-  {
-    public void Dispose() { }
-
-    public ILogger CreateLogger(string categoryName)
-    {
-      return new CustomConsoleLogger(categoryName);
-    }
-
-    public class CustomConsoleLogger : ILogger
-    {
-      private readonly string _categoryName;
-
-      public CustomConsoleLogger(string categoryName)
-      {
-        _categoryName = categoryName;
-      }
-
-      public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
-      {
-        if (!IsEnabled(logLevel))
-        {
-          return;
-        }
-
-        Console.WriteLine($"{logLevel}: {_categoryName}[{eventId.Id}]: {formatter(state, exception)}");
-      }
-
-      public bool IsEnabled(LogLevel logLevel)
-      {
-        return true;
-      }
-
-      public IDisposable BeginScope<TState>(TState state)
-      {
-        return null;
-      }
     }
   }
 }
