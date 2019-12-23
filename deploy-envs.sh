53c604b141675da365ab7bcae29d0fec7d7c9e88
Todd Robertson <todd@kwerius.com>
2019-12-22 18:09:39 -0800

    Initial config changes

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # set environment variables used in deploy.sh and AWS task-definition.json:
-export IMAGE_NAME=netcoreapps-redisgeo
+export IMAGE_NAME=ecsdemo-redisgeo
 export IMAGE_VERSION=latest
 
-export AWS_DEFAULT_REGION=us-east-1
+export AWS_DEFAULT_REGION=us-west-2
 export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=redisgeo.netcore.io
+export AWS_VIRTUAL_HOST=ec2-demo.kwerius.com
+export LETSENCRYPT_HOST=$AWS_VIRTUAL_HOST
+export LETSENCRYPT_EMAIL=app@kwerius.com
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
